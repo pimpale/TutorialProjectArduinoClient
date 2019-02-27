41dc064c61a69738095a970a2ed35726136538c0
pimpale <gpimpale29@gmail.com>
2019-02-26 20:48:59 -0800

    64 to 32 bit

--- testo.ino
+++ testo.ino
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <inttypes.h>
+#include <string.h>
 
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
@@ -9,31 +9,28 @@
 #include <user_interface.h>
 #include <wpa2_enterprise.h>
 
-
 /* create a super_secret_password_definitions.h that defines:
  * SUPER_SECRET_SSID
  * SUPER_SECRET_USERNAME
- * SUPER_SECRET_PASSWORD 
+ * SUPER_SECRET_PASSWORD
  */
 #include "super_secret_password_definition.h"
 
-
 #define ERR_OK 0
 #define ERR_NOMEM 1
-
 
 #define BAUD 9600
 #define PROTOCOL "http"
 #define HOST "99-103-193-239.lightspeed.sntcca.sbcglobal.net:8080"
 #define THIS_LOCATION 1
 
-
 MFRC522 mfrc522;  // Create MFRC522 instance
 MFRC522::MIFARE_Key key;
 
 void setup() {
-  Serial.begin(BAUD); 
-  while (!Serial);
+  Serial.begin(BAUD);
+  while (!Serial)
+    ;
   Serial.printf("setup: complete\n");
 
   initMFRC522(&mfrc522, &key);
@@ -69,29 +66,28 @@ void loop() {
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
-  status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(blockAddr, buffer, &size);
+  status = (MFRC522::StatusCode)mfrc522.MIFARE_Read(blockAddr, buffer, &size);
   if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("MIFARE_Read() failed: "));
-        Serial.println(mfrc522.GetStatusCodeName(status));
+    Serial.print(F("MIFARE_Read() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
   }
 
-  
-  uint64_t userId;
-  to_uint64_t(&userId, (uint8_t*)buffer);
+  uint32_t userId;
+  to_uint32_t(&userId, (uint8_t*)buffer);
 
   char encounterUrl[1024];
-  mkEncounterUrl(encounterUrl, 1024, PROTOCOL, HOST, "in", THIS_LOCATION, userId);
+  mkEncounterUrl(encounterUrl, 1024, PROTOCOL, HOST, "in", THIS_LOCATION,
+                 userId);
   httpGet(encounterUrl);
-  
+
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
 }
 
-void mkEncounterUrl(char* dest, size_t destlen, 
-                    char* protocol, char* host, char* eventType, 
-                    uint64_t locationId, uint64_t userId) {
-  snprintf(dest, destlen, "%s://%s/?type=%s&locationId=%" PRIu64 "&userId=%" PRIu64, 
-           protocol, host, eventType, locationId, userId);
+void mkEncounterUrl(char* dest, size_t destlen, char* protocol, char* host,
+                    char* eventType, uint32_t locationId, uint32_t userId) {
+  snprintf(dest, destlen, "%s://%s/?type=%s&locationId=%u&userId=%u", protocol,
+           host, eventType, locationId, userId);
 }
 
 void initMFRC522(MFRC522* mfrc522, MFRC522::MIFARE_Key* key) {
@@ -134,22 +130,23 @@ uint32_t wpaConnect(const char* ssid, const char* password) {
   char* mname = "wpaConnect";
   Serial.printf("%s: begin\n", mname);
   WiFi.begin(ssid, password);
-  while(true) {
-    if(WiFi.status() == WL_CONNECTED) {
-     Serial.printf("%s: connection succeeded\n", mname);
-     Serial.printf("%s: current ip address: %s\n", mname, WiFi.localIP().toString().c_str());
-     break;
-    }
-    else {
-     Serial.printf("%s: connection attempt failed, retrying\n", mname);
-     delay(500);
+  while (true) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.printf("%s: connection succeeded\n", mname);
+      Serial.printf("%s: current ip address: %s\n", mname,
+                    WiFi.localIP().toString().c_str());
+      break;
+    } else {
+      Serial.printf("%s: connection attempt failed, retrying\n", mname);
+      delay(500);
     }
   }
   Serial.printf("%s: complete\n", mname);
   return ERR_OK;
 }
 
-uint32_t enterpriseWpaConnect(const char* ssid, const char* username, const char* password) {
+uint32_t enterpriseWpaConnect(const char* ssid, const char* username,
+                              const char* password) {
   char* mname = "enterpriseWpaConnect";
 
   wifi_set_opmode(STATION_MODE);
@@ -189,13 +186,13 @@ void dump_char_array(char* buffer, char bufferSize) {
   }
 }
 
-void to_uint64_t(uint64_t* dest, uint8_t* src) {
+void to_uint32_t(uint32_t* dest, uint8_t* src) {
   *dest = (src[0] << 24) + (src[1] << 16) + (src[2] << 8) + src[3];
 }
 
-void to_uint8_t_ptr(uint8_t* dest, uint64_t* src) {
+void to_uint8_t_ptr(uint8_t* dest, uint32_t* src) {
   dest[0] = (*src >> 24) & 0xFF;
   dest[1] = (*src >> 16) & 0xFF;
-  dest[2] = (*src >> 8 ) & 0xFF;
-  dest[3] = (*src >> 0 ) & 0xFF;
+  dest[2] = (*src >> 8) & 0xFF;
+  dest[3] = (*src >> 0) & 0xFF;
 }
